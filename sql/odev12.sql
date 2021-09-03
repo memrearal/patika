5a6d3733d2425b4d39d7c47c7e80c5fa926a580a
Emre <50625747+memrearal@users.noreply.github.com>
2021-09-03 21:44:03 +0300

    Create odev12.sql

---
+++ sql/odev12.sql
@@ -0,0 +1,23 @@
+/*
+ Soru 1:
+ Film tablosunda film uzunluğu length sütununda gösterilmektedir. Uzunluğu ortalama film uzunluğundan fazla kaç tane film vardır?
+*/
+SELECT COUNT(*)  FROM film WHERE length > ( SELECT AVG(length) FROM film);
+
+/*
+ Soru 2:
+ Film tablosunda en yüksek rental_rate değerine sahip kaç tane film vardır?
+*/
+SELECT COUNT(*) FROM film WHERE rental_rate = ( SELECT MAX(rental_rate) FROM film);
+
+/*
+ Soru 3:
+ Film tablosunda en düşük rental_rate ve en düşün replacement_cost değerlerine sahip filmleri sıralayınız.
+*/
+SELECT title FROM film WHERE film_id = any ( SELECT film_id FROM film WHERE rental_rate = ( SELECT MIN(rental_rate ) FROM film ) AND replacement_cost = ( SELECT MIN(replacement_cost) FROM film ) );
+
+/*
+ Soru 4:
+ Payment tablosunda en fazla sayıda alışveriş yapan müşterileri(customer) sıralayınız.
+*/
+SELECT first_name,last_name FROM customer c JOIN payment p ON ( p.customer_id = c.customer_id ) WHERE amount = ( SELECT MAX(amount) from payment );
